2de985c8be99647831046ba7cb23a31b25abb4f2
MrHuxu <hxtheone@gmail.com>
2013-10-08 13:07:52 +0800

    near to finish this project

--- src/JavaMinesweeper.java
+++ src/JavaMinesweeper.java
@@ -2,7 +2,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.lang.*;
 import javax.swing.*;
-import javax.swing.event.*;
 import java.util.*;
 import java.util.List;
 
@@ -10,6 +9,7 @@ import java.util.List;
 public class JavaMinesweeper {
     public static int disableBtns = 0;
 
+    //when click the 0 button, use recursion to set 0 button around it disable
     public static void noMines(MineButton[] btns, int location, int width, int height) {
         if (!btns[location].isMine && btns[location].NumofMines > 0 && btns[location].bt.isEnabled()) {
             btns[location].bt.setText(Integer.toString(btns[location].NumofMines));
@@ -18,6 +18,7 @@ public class JavaMinesweeper {
             return;
         } else if (!btns[location].isMine && btns[location].NumofMines == 0) {
             if (btns[location].bt.isEnabled()) {
+                btns[location].bt.setText("");
                 btns[location].bt.setEnabled(false);
                 disableBtns++;
                 if (0 <= location && location <= (width - 1)) {
@@ -66,6 +67,14 @@ public class JavaMinesweeper {
         }
     }
 
+    //when game finished, show all mines
+    public static void showMines(MineButton[] btns, List mines) {
+        for (int i = 0; i < mines.size(); i++) {
+            btns[Integer.parseInt(mines.get(i).toString())].bt.setText("⚙");
+            btns[Integer.parseInt(mines.get(i).toString())].bt.setEnabled(false);
+        }
+    }
+
     public static void main(String[] args) {
         //the setting window
         final JFrame setting = new JFrame("Setting");
@@ -77,6 +86,8 @@ public class JavaMinesweeper {
         JLabel numlb = new JLabel("Number of mines: ");
         final TextField numtxt = new TextField();
         JButton start = new JButton("   Start!!   ");
+        start.setForeground(Color.red);
+        start.setVisible(true);
         setting.add(widthlb);
         setting.add(widthtxt);
         setting.add(heightlb);
@@ -88,7 +99,7 @@ public class JavaMinesweeper {
         //get the width and height of the screen, and make the window stay at the center of the screen by using the two piece of data
         final int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
         final int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
-        setting.setLocation((screenWidth - 200) / 2, (screenHeight - 320) / 2);
+        setting.setLocation((screenWidth - 250) / 2, (screenHeight - 320) / 2);
         setting.setSize(250, 140);
         setting.setResizable(false);
         setting.setVisible(true);
@@ -103,8 +114,26 @@ public class JavaMinesweeper {
                 Random rd = new Random();
                 final List mines = new ArrayList();
 
-                if (width < 0 || height < 0) {
-                    System.out.println("Illegal Input!!!");
+                //When data is error, show this frame
+                final JFrame illegal = new JFrame();
+                illegal.setLayout(new FlowLayout());
+                JLabel illegallb = new JLabel("Illegal Input!!!");
+                JButton illegalbtn = new JButton("Reset >>");
+                illegalbtn.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        illegal.dispose();
+                    }
+                });
+                illegal.add(illegallb);
+                illegal.add(illegalbtn);
+                illegal.setSize(150, 80);
+                illegal.setLocation((screenWidth - 150) / 2, (screenHeight - 300) / 2);
+                illegal.setResizable(false);
+
+                //Use the data to draw the chessboard
+                if (width < 3 || height < 3 || num < 0 || num > width * height) {
+                    illegal.setVisible(true);
                 } else {
                     setting.dispose();        //close the setting window
 
@@ -180,6 +209,7 @@ public class JavaMinesweeper {
                     final JFrame minespace = new JFrame("MineSpace");
                     minespace.setLayout(new GridLayout(height, width));
 
+                    //when game is finished, show this frame
                     final JFrame finish = new JFrame();
                     finish.setLayout(new FlowLayout());
                     JLabel restart = new JLabel("Quit??");
@@ -206,16 +236,49 @@ public class JavaMinesweeper {
                     finish.setLocation((screenWidth - 150) / 2, (screenHeight - 295) / 2);
                     finish.setResizable(false);
 
+                    //add the mouse right-click event to every button
+                    for (int i = 0; i < width * height; i++) {
+                        final int looptmp2 = i;
+                        btnArr[i].bt.addMouseListener(new MouseListener() {
+                            @Override
+                            public void mouseClicked(MouseEvent e) {
+                                if (InputEvent.BUTTON3_DOWN_MASK != 0 && btnArr[looptmp2].bt.isEnabled()) {
+                                    if (!btnArr[looptmp2].flag) {
+                                        btnArr[looptmp2].bt.setText("✘");
+                                        btnArr[looptmp2].flag = true;
+                                    } else {
+                                        btnArr[looptmp2].bt.setText("");
+                                        btnArr[looptmp2].flag= false;
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void mousePressed(MouseEvent e) {
+                            }
+
+                            @Override
+                            public void mouseReleased(MouseEvent e) {
+                            }
+
+                            @Override
+                            public void mouseEntered(MouseEvent e) {
+                            }
+
+                            @Override
+                            public void mouseExited(MouseEvent e) {
+                            }
+                        });
+                    }
+
+                    //add mouse left-click to every button
                     for (int i = 0; i < width * height; i++) {
                         final int looptmp = i;
                         if (btnArr[i].isMine) {
                             btnArr[i].bt.addActionListener(new ActionListener() {
                                 @Override
                                 public void actionPerformed(ActionEvent e) {
-                                    for (int j = 0; j < mines.size(); j++) {
-                                        btnArr[Integer.parseInt(mines.get(j).toString())].bt.setText("⚙");
-                                        btnArr[Integer.parseInt(mines.get(j).toString())].bt.setEnabled(false);
-                                    }
+                                    showMines(btnArr, mines);
                                     finish.setTitle("You LOSE!!");
                                     finish.setVisible(true);
                                 }
@@ -228,6 +291,7 @@ public class JavaMinesweeper {
                                     btnArr[looptmp].bt.setEnabled(false);
                                     disableBtns++;
                                     if (disableBtns == width * height - num) {
+                                        showMines(btnArr, mines);
                                         finish.setTitle("You WIN");
                                         finish.setVisible(true);
                                     }
@@ -239,6 +303,7 @@ public class JavaMinesweeper {
                                 public void actionPerformed(ActionEvent e) {
                                     noMines(btnArr, looptmp, width, height);
                                     if (disableBtns == width * height - num) {
+                                        showMines(btnArr, mines);
                                         finish.setTitle("You WIN");
                                         finish.setVisible(true);
                                     }
